b5d09db2047576aa41faf1eb09207d8a7c97be7e
wuhao <wuhao1@rongcloud.cn>
2021-12-03 19:18:41 +0800

    add run.sh

--- source/_posts/devops/docker/docker-package/dockerfiles/ubuntu/monitor/run.sh
+++ source/_posts/devops/docker/docker-package/dockerfiles/ubuntu/monitor/run.sh
@@ -1,9 +1,21 @@
+
+
+docker commit 281bcc444fb0 wuhaocn/monitor:1.0.2
+docker tag wuhaocn/monitor:1.0.2 wuhaocn/monitor:1.0.2
+docker push wuhaocn/monitor:1.0.2
+
 docker stop monitor-test
 docker rm monitor-test
-docker run --name monitor-test --privileged=true  -p 13000:3000 -p 18500:8500  -p 19090:9090 -itd wuhaocn/monitor:1.0.0
+docker run --name monitor-test --privileged=true  -p 13000:3000 -p 18500:8500  -p 19090:9090 -itd wuhaocn/monitor:1.0.2 bash /monitor/start.sh
 
-docker commit 81a82e9b5ac2 wuhaocn/monitor:1.0.0
 
-docker tag wuhaocn/monitor:1.0.0 wuhaocn/monitor:1.0.0
 
-docker push wuhaocn/monitor:1.0.0
+
+wget https://github.com/prometheus/node_exporter/releases/download/v1.3.0/node_exporter-1.3.0.darwin-amd64.tar.gz
+
+http://127.0.0.1:9100/metrics
+
+
+
+curl -X PUT -d '{"id": "nodeInfo","name": "nodeInfo","address": "10.3.1.226","port": 9100,"tags": ["dev"],"checks": [{"http": "http://10.3.1.226:9100/","interval": "5s"}]}'     http://localhost:18500/v1/agent/service/register
+
